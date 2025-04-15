0b8273ed7c5115da30ff6cf7a6051bb5865c0fab
kashvigarg <kashvigarg02@gmail.com>
2025-04-15 17:05:33 +0530

    Add Drag/Drop Upload Feature

---
+++ src/components/ui/DragDrop.js
@@ -0,0 +1,111 @@
+import React, { useState, useRef } from "react";
+import formatFileSize from "./helper";
+
+const DragDropComponent = () => {
+  const [isDragging, setDragging] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedFile(file);
+    }
+  };
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDragLeave = () => {
+    setDragging(false);
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+
+    const files = e.dataTransfer.files;
+    if (files && files.length > 0) {
+      setSelectedFile(files[0]);
+    }
+  };
+
+  const removeSelectedFile = () => {
+    // fileInputRef.current.value = null; // Reset file input value
+    setSelectedFile(null);
+  };
+
+  return (
+    <div 
+      className={`file-drop-zone ${isDragging ? "dragging" : ""}`}
+      onDragEnter={handleDragEnter}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+    >
+      {!selectedFile ? (
+        <>
+          <img
+            src="./icons/upload.png"
+            alt=""
+            style={{ height: "30%", width: "auto", margin: "20px" }}
+          />
+          <br />
+          Drag and Drop or{" "}
+          <button onClick={() => fileInputRef.current.click()}>Upload a Video</button>
+          <input
+            type="file"
+            accept="video/*"
+            ref={fileInputRef}
+            style={{ display: "none" }}
+            onChange={handleFileUpload}
+          />
+        </>
+      ) : (
+        <div style={{ position: "relative", display: "inline-block", marginTop: "10px" }}>
+          <video
+            src={URL.createObjectURL(selectedFile)}
+            controls
+            style={{
+                borderRadius: "10px",
+                // minHeight: "30%",
+                // maxHeight: "50%",
+                maxWidth: "40vw",
+                width: "auto",
+                height: "auto",
+                objectFit: "contain"
+              }}
+          />
+          <button
+            onClick={removeSelectedFile}
+            style={{
+              position: "absolute",
+              top: "-10px",
+              right: "-10px",
+              background: "red",
+              color: "#fff",
+              border: "none",
+              borderRadius: "50%",
+              width: "25px",
+              height: "25px",
+              cursor: "pointer",
+              fontWeight: "bold",
+            }}
+          >
+          </button>
+          <div style={{ marginTop: "10px", fontSize: "14px", color: "#444" }}>
+            {`Size: ${formatFileSize(selectedFile.size)}`}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DragDropComponent;
---
+++ src/components/ui/helper.js
@@ -0,0 +1,8 @@
+export default function formatFileSize(bytes) {
+    if (bytes === 0) return '0 Bytes';
+    const k = 1024;
+    // const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const size = 'MB'
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + size;
+  }
