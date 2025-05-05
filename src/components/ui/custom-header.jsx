66070958afd5a7602c6b7ef4933685e1e75db721
kashvigarg <kashvigarg02@gmail.com>
2025-05-05 15:20:29 +0530

    Add custom header + loader

---
+++ src/components/ui/custom-header.jsx
@@ -0,0 +1,32 @@
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { logout } from '../../store/authSlice';
+
+const CustomHeader = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const goToLoc = (loc) =>{
+    navigate(loc);
+  }
+
+  const logoutUser = () => {
+    dispatch(logout());                   
+  }
+
+    return (
+        <div className="dashboard-header">
+        { location.pathname === "/dashboard" ?
+        <button onClick={() => goToLoc("/profile")}>Profile</button> : null }
+        { location.pathname === "/profile" ?
+                <button onClick={() => goToLoc("/dashboard")}>Dashboard</button> : null}
+        <h1 className="sub-heading">V-Trance</h1>
+        <button onClick={() => logoutUser()} className="logout-button">
+          <img src="./icons/logout.png" alt="" className="logout-icon" />
+          Logout
+        </button>
+      </div>
+    );
+}
+
+export default CustomHeader;
---
+++ src/components/ui/loading-widget.jsx
@@ -0,0 +1,9 @@
+import '../../App.css'
+
+const CustomLoader = () => (
+  <div style={{ display: "flex", justifyContent: "center", marginTop: "2rem" }}>
+    <div className="spinner"></div>
+  </div>
+);
+
+export default CustomLoader;
