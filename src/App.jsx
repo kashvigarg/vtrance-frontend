d70d60ac60f69997dda6ed55c0f107738a953ce3
kashvigarg <kashvigarg02@gmail.com>
2025-05-13 13:52:49 +0530

    Refactor Code

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,23 @@ import PrivateRoute from "./router/route";
 import Landing from './components/Landing';
 import { ToastContainer } from 'react-toastify';
 import Profile from './components/Profile';
+import Video from './components/Video';
 
 function App() {
   return (
-      <Router>
+    <Router>
       <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            {/* <PrivateRoute path="/dashboard" element={<Dashboard />} /> */}
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+        <Route path="/" element={<Landing />} />
+        {/* <Route exact path='/dashboard' element={<PrivateRoute />}> */}
+          <Route exact path='/dashboard' element={<Dashboard />} />
+        {/* </Route> */}
+        {/* <Route exact path='/profile' element={<PrivateRoute />}> */}
+          <Route exact path='/profile' element={<Profile />} />
+        {/* </Route> */}
+        <Route path="/video" element={<Video />} />
+      </Routes>
       <ToastContainer />
-      </Router>
+    </Router>
   );
 }
 
